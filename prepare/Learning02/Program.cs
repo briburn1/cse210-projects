6add0da2cf34c466f72d662a3e923fdd9e9ce51f
briburn1 <bhannah767@yahoo.com>
2023-01-18 18:44:52 -0700

    added code to program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "dd's Discounts";
+        job1._jobTitle = "Area Supervisor";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        
+        Job job2 = new Job();
+        job2._company = "ATSU";
+        job2._jobTitle = "Full Time Student";
+        job2._startYear = 2022;
+        job2._endYear = 2025;
+
+        Resume myResume = new Resume();
+        myResume._name = "Brianna Burnham";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
